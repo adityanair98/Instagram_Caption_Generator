b6392d4d5be6ed5e91ab552651f1f6236b0cb514
cjrich19 <112117929+cjrich19@users.noreply.github.com>
2024-05-04 12:48:20 -0700

    Add files via upload
    pep-8 version

--- application/✨_Home.py
+++ application/✨_Home.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from pathlib import Path
 
+
 # Function to store the user data to a CSV file
 def save_user_data(first_name, last_name, email, phone):
     """Function to store the user data to a CSV file"""
@@ -10,16 +11,20 @@ def save_user_data(first_name, last_name, email, phone):
     if csv_file.exists():
         df = pd.read_csv(csv_file)
     else:
-        df = pd.DataFrame(columns=["First Name", "Last Name", "Email", "Phone Number"])
+        df = pd.DataFrame(
+            columns=["First Name", "Last Name", "Email", "Phone Number"])
 
     # Add and save new user data
-    new_data = {"First Name": first_name, "Last Name": last_name, "Email": email, "Phone Number": phone}
+    new_data = {"First Name": first_name, "Last Name": last_name,
+                "Email": email, "Phone Number": phone}
     df = df.append(new_data, ignore_index=True)
     df.to_csv(csv_file, index=False)
 
+
 def main():
     # Define Streamlit configurations
-    st.set_page_config(page_title="Instamuse", page_icon=":camera:", initial_sidebar_state='auto')
+    st.set_page_config(page_title="Instamuse", page_icon=":camera:",
+                       initial_sidebar_state='auto')
 
     # Sidebar configuration
     with st.sidebar:
@@ -27,7 +32,8 @@ def main():
         st.title("InstaMuse")
         st.subheader("Welcome to InstaMuse, the ultimate tool for turning\
         your snapshots into social media sensations!")
-        st.write("Start turning heads with your posts. Use InstaMuse now and watch your likes soar!")
+        st.write("Start turning heads with your posts.\
+                 Use InstaMuse now and watch your likes soar!")
 
     # Title
     st.markdown("""
@@ -35,25 +41,28 @@ def main():
     .title-font {
         font-size: 50px;
         font-weight: bold;
-        font-family: 'Helvetica', sans-serif; /* You can change the font-family */
+        font-family: 'Helvetica', sans-serif; /* You can change the\
+                font-family */
         color: #FF573D; /* Color for the title */
     }
     </style>
     """, unsafe_allow_html=True)
-    st.markdown('<h1 class="title-font">The Worlds Best Instagram Caption Generator</h1>', unsafe_allow_html=True)
+    st.markdown('<h1 class="title-font">The Worlds Best Instagram Caption\
+                Generator</h1>', unsafe_allow_html=True)
 
     # Subheader
     st.write("""### Transform your online presence—join InstaMuse to create \
         innovative captions that increase likes and enhance your visibility.\
         """)
-    
+
     # Gif
     st.image('insta.gif')
 
     # Why us?
     st.subheader('Why InstaMuse?')
     st.write("""
-    - 🎨 **Creativity at your fingertips:** Generate unique captions that stand out.
+    - 🎨 **Creativity at your fingertips:** Generate unique captions that\
+             stand out.
     - ⏳ **Save time:** Stop spending hours trying to write the perfect caption.
     - 🚀 **Boost engagement:** Captions crafted to keep your audience engaged.
     - 💸 **Free to use:** Get access to our technology preview at no cost.
@@ -78,28 +87,33 @@ def main():
         st.write('"The creative captions we developed with InstaMuse \
                  have not only increased likes but also built lasting \
                  connections with our followers." - MK')
-    
+
     # Sign up section
     st.subheader('Sign up here for priority access')
-    st.write('Enter your details below to get early access to our innovative caption generator.')
+    st.write('Enter your details below to get early access to our innovative\
+             caption generator.')
 
     # Form to collect user details
     with st.form(key='user_details_form'):
         cols = st.columns(2)
         with cols[0]:
             first_name = st.text_input("First Name")
-            email = st.text_input("Email", help="We'll never share your email with anyone else.")
+            email = st.text_input("Email", help="We'll never share your email\
+                                  with anyone else.")
         with cols[1]:
             last_name = st.text_input("Last Name")
-            phone = st.text_input("Phone Number", help="We'll never share your number with anyone else.")
+            phone = st.text_input("Phone Number", help="We'll never share your\
+                                  number with anyone else.")
         submit_button = st.form_submit_button("Start Using InstaMuse")
-        
+
         if submit_button:
             if first_name and last_name and email and phone:
                 save_user_data(first_name, last_name, email, phone)
-                st.success("Thank you for signing up! We will be in touch soon.")
+                st.success("Thank you for signing up! We will be in\
+                           touch soon.")
             else:
                 st.error("Please fill in all the fields.")
+
 
 if __name__ == "__main__":
     main()
