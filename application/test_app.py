31b3f2042225ded24d00e8df988dde8b0d3f4864
cjrich19 <112117929+cjrich19@users.noreply.github.com>
2024-05-04 12:45:43 -0700

    Add files via upload
    pep-8 version

--- application/test_app.py
+++ application/test_app.py
@@ -2,6 +2,7 @@ import unittest
 from PIL import Image
 from app import import_and_predict
 
+
 class TestApp(unittest.TestCase):
 
     def test_import_and_predict(self):
@@ -9,6 +10,7 @@ class TestApp(unittest.TestCase):
         caption = import_and_predict(image_data)  # Call the function
         self.assertIsInstance(caption, str)  # Check if caption is a string
         self.assertTrue(len(caption) > 0)  # Check if caption is not empty
+
 
 if __name__ == '__main__':
     unittest.main()
