7023b36e00af42933298513faa2b0c9ca498bd3e
Seneth Waterman <137919843+seneth-waterman@users.noreply.github.com>
2024-05-05 18:17:23 -0700

    Update ✨_Home.py

--- application/✨_Home.py
+++ application/✨_Home.py
@@ -76,7 +76,6 @@ def main():
         
         if submit_button:
             if first_name and last_name and email and phone:
-                # Uncomment the following line to save user data
                 save_user_data(first_name, last_name, email, phone)
                 st.success("Thank you for signing up! We will be in touch soon.")
             else:
